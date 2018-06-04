7252a2ebed9d340d6e118c02a61e4d9c27856927
Olivier Mangin <aicel@make-it.fr>
2018-06-04 17:15:59 +0200

    add annimation and redirection

--- imports/pages/general/ResultsAnimate.js
+++ imports/pages/general/ResultsAnimate.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 
 class ResultsAnimate extends Component {
-  state = {
+  endAnimate = () => {
+    Meteor.call('configuration.finish_animation');
+    this.props.history.push('/results');
   }
 
   render = () => {
-    
+    setTimeout(this.endAnimate, 13000)
     return (
-      <h1>Results Animation</h1>
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          background: "url('/images/results-countdown.gif') center center / cover no-repeat"
+        }}
+      />
     );
   }
 }
