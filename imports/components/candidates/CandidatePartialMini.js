57324baa1b469bd821a50a93e55a52905f7856ff
Jimmy <jeremy.simonklein@gmail.com>
2018-06-06 17:02:50 +0200

    Added nb votes on CandidatePartialMini

--- imports/components/candidates/CandidatePartialMini.js
+++ imports/components/candidates/CandidatePartialMini.js
@@ -15,8 +15,8 @@ import _ from 'lodash'
     }
 */
 
-const CandidatePartialMini = ({ candidate, className }) => (
-  <div className={className + " wow fadeInUp "}>
+const CandidatePartialMini = ({ candidate, className, nbVotes }) => (
+  <div className={className + " animated fadeInUp "}>
       <div
         className="avatar"
         style={{background: `url(${candidate.image_url}) no-repeat center center`}}
@@ -24,7 +24,8 @@ const CandidatePartialMini = ({ candidate, className }) => (
       />
       <div className="identity">
         <h3>{candidate.lastname}</h3>
-        <p>{candidate.firstname}</p>
+        <p className="firstname">{candidate.firstname}</p>
+        <p className="total-votes">{candidate.votes} votes</p>
       </div>
   </div>
 );
@@ -33,7 +34,7 @@ export default styled(CandidatePartialMini)`
     background-color: #2699FB;
     width: 30em;
     margin: auto;
-    padding: 2em;
+    padding: 1em 2em 2em 2em;
     margin-bottom: 3em;
     border: 15px solid ${({voted}) => voted ? '#B8FFFC' : 'white'};
     border-top-left-radius: 5em;
@@ -63,6 +64,12 @@ export default styled(CandidatePartialMini)`
       > h3 {
         margin-bottom: 0;
         font-size: 
+      }
+      > .firstname {
+        margin-bottom: 0;
+      }
+      > .total-votes{
+        font-size: 0.6em;
       }
     }
 `;
